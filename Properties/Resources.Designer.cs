f2377bfabbc0035a6ec864d71a63ddf66d3138c2
Bendix988 <80847220+Bendix988@users.noreply.github.com>
2021-12-04 18:38:56 +0100

    Update Resources.Designer.cs

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace PTLauncher.Properties {
+namespace XVLauncher.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace PTLauncher.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PTLauncher.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("XVLauncher.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
